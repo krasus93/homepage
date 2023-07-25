141df2691ff63d8210c44212b7dee8e633a11598
Marksonfd <>
2023-07-25 20:45:21 +0200

    bem2&js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 alert ('Siema, tu Marek!')
+
+
+const firstName = 'Marek';
+const age = 30;
+
+console.log(`Jestem ${firstName} i mam ${age} lat`)
+
+const header = document.querySelector('.head__box--surprise--js')
+
+
+
+header.innerHTML = 'Mam Super Moce!'
