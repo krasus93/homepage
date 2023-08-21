59853d97d19a5ecfe8ca2b7cc482a6e5215f93f4
Marksonfd <>
2023-08-21 20:50:44 +0200

    add hamburger

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
 
 
 
-
 function welcome (Name, age){
 
     console.log(`Jestem ${Name} i mam ${age} lat`);
@@ -18,6 +17,17 @@ welcome ("pioter",30);
 welcome ("justa",40);
 welcome ("dzerry",50);
 
+
+
+
+const hamburger = document.querySelector ('.hamburger-menu-js')
+
+console.log(hamburger)
+
+hamburger.addEventListener('click', () => {
+    const navigation = document.querySelector('.navigation-box--js')
+    navigation.classList.toggle('navigation--open')
+})
 
 
 
