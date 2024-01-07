5c171a748ef3bad8dae1b278a77f602fdf3f5e46
Marksonfd <>
2024-01-07 13:55:01 +0100

    add agregat array

---
+++ src/js/agregatearray.js
@@ -0,0 +1,7 @@
+const tab = [134,144,43,556];
+let sum = 0;
+for (let i=0; i<tab.length; i++) {
+    sum += tab[i];
+};
+
+console.log(sum);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 import { hamburger, myFavouriteColour } from "./hamburger.js";
+import { differenceInDays } from "date-fns";
 
+const resultDays = differenceInDays(
+      new Date(2024, 1, 24, 0, 0),
+      new Date(2024, 0, 3, 23, 0)
+     )
+
+     console.log (resultDays)
 
 console.log (myFavouriteColour);
 
@@ -23,6 +30,14 @@ hamburger.addEventListener('click', () => {
     navigation.classList.toggle('navigation--open')
 })
 
+
+const tab = [134,144,43,556];
+let sum = 0;
+for (let i=0; i<tab.length; i++) {
+    sum += tab[i];
+};
+
+console.log(sum);
 
 
 
